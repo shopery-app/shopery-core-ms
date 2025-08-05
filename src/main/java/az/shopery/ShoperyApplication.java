46b325e6c2f1b65c4280cf3983a648189b5d5c3d
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2025-08-05 20:46:06 +0400

    project setup

---
+++ src/main/java/az/shopery/ShoperyApplication.java
@@ -0,0 +1,13 @@
+package az.shopery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoperyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoperyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/az/shopery/ShoperyApplicationTests.java
@@ -0,0 +1,13 @@
+package az.shopery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoperyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
