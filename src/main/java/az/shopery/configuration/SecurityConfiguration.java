c8643822accbafd366c3fd3a78761ec308618d3e
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2025-08-08 04:45:28 +0400

    feature-004: fix bugs

--- src/main/java/az/shopery/configuration/SecurityConfiguration.java
+++ src/main/java/az/shopery/configuration/SecurityConfiguration.java
@@ -47,7 +47,10 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://shopery.pages.dev"
+        ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "x-auth-token"));
         configuration.setAllowCredentials(true);
