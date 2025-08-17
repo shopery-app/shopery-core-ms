885f74dc26825c36ca57d79c7f6d1fef414a555d
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2025-08-17 21:48:48 +0400

    feature-027: fix ambiguous url

--- src/main/java/az/shopery/controller/ShopController.java
+++ src/main/java/az/shopery/controller/ShopController.java
@@ -10,10 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/shops")
@@ -33,13 +30,13 @@ public class ShopController {
         return ResponseEntity.ok(shopService.getAllShops(pageable));
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopById(@PathVariable String id) {
+    @GetMapping
+    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopById(@RequestParam String id) {
         return ResponseEntity.ok(shopService.getShopById(id));
     }
 
-    @GetMapping("/{name}")
-    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopByShopName(@PathVariable String name) {
+    @GetMapping
+    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopByShopName(@RequestParam String name) {
         return ResponseEntity.ok(shopService.getShopByShopName(name));
     }
 }
