dd96aaed72f0b0c694553e05f669812578ab7573
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2025-08-19 00:13:25 +0400

    bug fix

--- src/main/java/az/shopery/controller/ShopController.java
+++ src/main/java/az/shopery/controller/ShopController.java
@@ -1,16 +1,18 @@
 package az.shopery.controller;
 
+import az.shopery.model.dto.response.ShopResponseDto;
 import az.shopery.model.dto.response.SuccessResponseDto;
 import az.shopery.model.dto.response.UserShopResponseDto;
 import az.shopery.service.ShopService;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,16 +29,17 @@ public class ShopController {
     }
 
     @GetMapping
-    public ResponseEntity<?> getAllOrSearchShops(
-            @RequestParam(required = false) String id,
-            @RequestParam(required = false) String name,
-            Pageable pageable) {
-        if (id != null) {
-            return ResponseEntity.ok(shopService.getShopById(id));
-        }
-        if (name != null && !name.isBlank()) {
-            return ResponseEntity.ok(shopService.getShopByShopName(name));
-        }
+    public ResponseEntity<SuccessResponseDto<Page<ShopResponseDto>>> getAllOrSearchShops(Pageable pageable) {
         return ResponseEntity.ok(shopService.getAllShops(pageable));
+    }
+
+    @GetMapping("/name/{shopName}")
+    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopByShopName(@PathVariable String shopName) {
+        return ResponseEntity.ok(shopService.getShopByShopName(shopName));
+    }
+
+    @GetMapping("/id/{shopId}")
+    public ResponseEntity<SuccessResponseDto<ShopResponseDto>> getShopById(@PathVariable String shopId) {
+        return ResponseEntity.ok(shopService.getShopById(shopId));
     }
 }
