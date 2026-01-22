55b81f27eae1812fd8d17d15a58c493078ad1738
010816 <Jahangir.Alisoy@abb-bank.az>
2026-01-22 15:51:50 +0400

    small changes

--- src/main/java/az/shopery/model/entity/SavedBlogEntity.java
+++ src/main/java/az/shopery/model/entity/SavedBlogEntity.java
@@ -11,6 +11,8 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+import java.time.Instant;
+import java.util.UUID;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,16 +23,9 @@ import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.UuidGenerator;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import java.time.Instant;
-import java.util.UUID;
 
 @Entity
-@Table(
-        name = "saved_blogs",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"user_id", "blog_id"})
-        }
-)
+@Table(name = "saved_blogs", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "blog_id"})})
 @EntityListeners(AuditingEntityListener.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Getter
@@ -39,20 +34,16 @@ import java.util.UUID;
 @NoArgsConstructor
 @Builder
 public class SavedBlogEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @UuidGenerator
     UUID id;
-
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     UserEntity user;
-
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "blog_id", nullable = false)
     BlogEntity blog;
-
     @CreatedDate
     @Column(name = "saved_at", nullable = false, updatable = false)
     Instant savedAt;
