e895e0a1400925f02c89e02dd7bac7a1643d35ce
Nijat Alizada <161605416+NicatAliyevh@users.noreply.github.com>
2026-01-22 20:46:06 +0400

    feature-084: adding archive blog logic

--- src/main/java/az/shopery/repository/BlogRepository.java
+++ src/main/java/az/shopery/repository/BlogRepository.java
@@ -4,7 +4,6 @@ import az.shopery.model.entity.BlogEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 import java.util.UUID;
 
