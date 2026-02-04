3eed091a08d577f2e919800c00f40d4f7f77ecea
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-02-05 01:08:45 +0400

    feature-100: return presigned url of blog and author images

--- src/main/java/az/shopery/mapper/BlogMapper.java
+++ src/main/java/az/shopery/mapper/BlogMapper.java
@@ -4,6 +4,7 @@ import az.shopery.model.dto.response.BlogResponseDto;
 import az.shopery.model.dto.shared.AuthorDto;
 import az.shopery.model.entity.BlogEntity;
 import az.shopery.repository.BlogLikeRepository;
+import az.shopery.utils.aws.S3FileUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,19 +13,20 @@ import org.springframework.stereotype.Component;
 public class BlogMapper {
 
     private final BlogLikeRepository blogLikeRepository;
+    private final S3FileUtil s3FileUtil;
 
     public BlogResponseDto toDto(BlogEntity blogEntity) {
         return BlogResponseDto.builder()
                 .id(blogEntity.getId())
                 .blogTitle(blogEntity.getBlogTitle())
                 .content(blogEntity.getContent())
-                .imageUrl(blogEntity.getImageUrl())
+                .imageUrl(s3FileUtil.generatePresignedUrl(blogEntity.getImageUrl()))
                 .createdAt(blogEntity.getCreatedAt())
                 .updatedAt(blogEntity.getUpdatedAt())
                 .likeCount(blogLikeRepository.countByBlog(blogEntity))
                 .author(AuthorDto.builder()
                         .name(blogEntity.getUser().getName())
-                        .profilePhotoUrl(blogEntity.getUser().getProfilePhotoUrl())
+                        .profilePhotoUrl(s3FileUtil.generatePresignedUrl(blogEntity.getUser().getProfilePhotoUrl()))
                         .build())
                 .build();
     }
