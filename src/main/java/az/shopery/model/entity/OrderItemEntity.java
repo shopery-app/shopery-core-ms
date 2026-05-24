ed9da429e201865d4a4a8b3921f5eae7fc1a7a86
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-05-25 03:10:27 +0400

    migration: move schedulers to batch-ms

--- src/main/java/az/shopery/model/entity/OrderItemEntity.java
+++ src/main/java/az/shopery/model/entity/OrderItemEntity.java
@@ -1,9 +1,22 @@
 package az.shopery.model.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.math.BigDecimal;
 import java.util.UUID;
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import org.hibernate.annotations.UuidGenerator;
 
